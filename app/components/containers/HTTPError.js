0af1435cc2d10aca1ba92e17d577ef87a2bbb675
Timothy Kua <timothyk7@gmail.com>
2017-07-09 22:05:34 -0700

    WIP still rough

--- app/components/containers/HTTPError.js
+++ app/components/containers/HTTPError.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Hangman from '../Hangman';
+// import Hangman from '../Hangman';
 
 const HTTPError = ({ code }) => (
   <div>
     <h1>Error {code}</h1>
     <h2>Hangman</h2>
     <p>Use the alphabet below to guess the word, or click hint to get a clue.</p>
-    <Hangman/>
   </div>
 );
 
