1ed30aa2e84cd790608c22343dbc6691c105380b
Timothy Kua <timothyk7@gmail.com>
2017-07-16 21:09:12 -0700

    adding subtitle to make 404 more user friendly

--- app/components/containers/HTTPError.js
+++ app/components/containers/HTTPError.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 const HTTPError = ({ code }) => (
   <section>
-    <h1 className="error-title">Error {code} </h1>
-    <div className="error-img-container">
-      <img src='/media/images/empty_box.svg' alt="empty box"/>
+    <div className='error-title'>
+      <h1>Error {code} </h1>
+      <h6> Sorry, Not Found</h6>
     </div>
-    <p className="error-credit">Icons made by
-      <a href="http://www.freepik.com" title="Freepik"> Freepik</a> from
-      <a href="http://www.flaticon.com" title="Flaticon"> www.flaticon.com</a> is licensed by
-      <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank"> CC 3.0 BY</a>
+    <div className='error-img-container'>
+      <img src='/media/images/empty_box.svg' alt='empty box'/>
+    </div>
+    <p className='error-credit'>Icons made by&nbsp;
+      <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from&nbsp;
+      <a href='http://www.flaticon.com' title='Flaticon'>www.flaticon.com</a> is licensed by&nbsp;
+      <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0
+        BY</a>
     </p>
   </section>
 );
